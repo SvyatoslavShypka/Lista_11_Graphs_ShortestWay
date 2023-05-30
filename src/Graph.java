2970042bfcbfc49456cf76c42e6484ae01d1d607
Svyatoslav Shypka <svyat200909@gmail.com>
2023-05-30 13:27:22 +0200

    Initial commit

---
+++ src/Graph.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Graph<T> {
+    public Graph(List<Edge<T>> edges) {
+        // TODO: Konstruktor
+    }
+
+    public Map<T, Integer> calculateShortestPaths(T startNode) throws NoSuchElementException {
+        // TODO: Wylicz najkrótsze ścieżki do każdego wierzchołka w grafie (Dijkstra)
+        return null;
+    }
+}
